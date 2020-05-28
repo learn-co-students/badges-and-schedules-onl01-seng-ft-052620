236c990db844e48627ee81250c797fcc8a1d1ab4
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-05-28 13:46:18 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+badges = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+# badges.each do |badge|
+   # puts "Hello, my name is #{badge}."
+ ##end
+  
+
